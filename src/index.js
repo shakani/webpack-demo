445853240a082d85f006b45b42e62d0008a9f02e
Sami Hakani <sami.hakani@gmail.com>
2023-09-09 14:31:32 -0400

    Initial commits

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+// import myName from './myName';
+import './style.css'
+import Icon from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.textContent = myName('Cody');
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+
+    return element;
+}
+
+document.body.appendChild(component());
